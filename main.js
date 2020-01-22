ee6cd0dee2b6b4500b02cf6fbb27d2c0345a22c7
CarlVinggaard <carl.vinggaard@hotmail.com>
2020-01-22 01:55:06 +0000

    Add getData and getRequestURL methods to app

--- main.js
+++ main.js
@@ -1,48 +1,91 @@
-// Define a new component called todo-item
-Vue.component('todo-item', {
-  props: ['todo'],
-  template: '<li>{{ todo.text }}</li>'
-});
-
-// Key for the MovieDatabase API
-var APIkey = 'b330fd993b7bc007e1e8713b02dc45f7';
-
-const baseURL = 'https://api.themoviedb.org/3/discover/movie?api_key='
-
-// Use XMLHttpRequest to call the API and parse the JSON object
-function getData(url, cb) {
-    var xhr = new XMLHttpRequest();
-
-    xhr.open("GET", url);
-    xhr.send();
-
-    // readyState == 4 means the request is completed, status == 200 means it is successful
-    xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            cb(JSON.parse(this.responseText));
-        };
-    };
-};
-
-fetchDataFromTMDB = function(baseURL) {
-    var url = 'baseURL' + 'APIkey' + '&';
-};
+// This does something related to Vue
+"use strict"
 
 var app = new Vue({
     el: '#app',
     data: {
         searchString: '',
-        year: '',
-        minrate: '',
-        maxrate: '',
-        genre: ''
+        releaseYear: '',
+        minRating: '',
+        maxRating: '',
+        genre: '',
+        sortBy: '',
+        results: ''
+    },
+        methods: { 
+        // Create request URL from input
+        getRequestURL: function() {
+            // Key for the MovieDatabase API
+            const APIkey = 'b330fd993b7bc007e1e8713b02dc45f7';
+            const baseURL = 'https://api.themoviedb.org/3/discover/movie?api_key='
+            var url = baseURL + APIkey;
+            
+            if (this.genre) {
+                var genreID = getGenreID(this.genre, genres); // Genres are identified with numbers, mapped in object in genres.js
+            }
+
+            /*if (searchString) {
+                url += '&' + 'with_keywords=' + 'app.searchString;
+            }*/
+
+            if (this.releaseYear) {
+                url += '&' + 'primary_release_year=' + this.releaseYear;
+                console.log("Year was set");
+            }
+
+            if (this.minRating) {
+                url += '&' + 'vote_average.lte=' + this.minRating;
+                console.log("minRating was set");
+            }
+            
+            // Minimum and Maximum don't seem to work together?
+            /*if (this.maxRating) {
+                url += '&' + 'vote_average.gte=' + this.maxRating;
+                console.log("maxRating was set");
+            }*/
+
+            if (genreID) {
+                url += '&' + 'with_genres=' + genreID;
+                console.log("Genre was set");
+            }
+
+            if (this.sortBy) {
+                url += '&' + 'sort_by=' + this.sortBy;
+                console.log("SortBy was set");
+            }
+
+            console.log(url);
+
+            return url;
+        },
+        getData: function() {
+            let url = this.getRequestURL();
+
+            axios.get(url)
+            .then(response => {
+                if (response.request.readyState == 4 && response.status == 200) {
+                    console.log(response);
+                    this.results = response.data;
+                }
+            });
+        }
     }
 });
 
+// A function that loops through the genres object (in genres.js) and return the ID
+function getGenreID(genreString, genres) {
+    
+    for (const genre of genres) {
+	    if (genreString.toLowerCase() == genre.name.toLowerCase()) {
+            return genre.id;
+        }
+    }
 
+    return false;
+}
 
-
-
+// TEST URL
+// 'https://api.themoviedb.org/3/discover/movie?api_key=b330fd993b7bc007e1e8713b02dc45f7&primary_release_year=2016&with_genres=10749&sort_by=popularity.desc'
 
 
 /* --------------------------------------------- */
@@ -77,18 +120,5 @@ var app5 = new Vue({
         reverseMessage: function () {
         this.message = this.message.split('').reverse().join('')
         }
-    }
-});
-
-
-
-var app7 = new Vue({
-    el: '#app-7',
-    data: {
-        groceryList: [
-        { id: 0, text: 'Vegetables' },
-        { id: 1, text: 'Cheese' },
-        { id: 2, text: 'Whatever else humans are supposed to eat' }
-        ]
     }
 });
